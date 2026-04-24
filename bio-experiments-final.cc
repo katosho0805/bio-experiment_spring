9e9ba6108c44c00756530528dea9ed0a5cdcfa4d
katosho0805 <ss.kato20030805@keio.jp>
2026-04-24 05:40:09 +0000

    完成

--- bio-experiments-final.cc
+++ bio-experiments-final.cc
@@ -75,7 +75,7 @@ void DivideDataset(const vector<vector<double>>& dataset,
         index.push_back(i);
       }
     // 乱数生成
-    mt19937 gen(42);
+    mt19937 gen(0);
 
     // シャッフル実行
     shuffle(index.begin(), index.end(), gen);
@@ -95,7 +95,9 @@ double GiniImpurity(const vector<int>& labs) {
     int n = labs.size();
     int l = 0;
     for(int i = 0; i < (int)labs.size(); i++){
-        if(labs[i] == 1) l++;
+        if(labs[i] == 1){
+            l++;
+        }
     }
     double p1 = (double)l / n;
     double p0 = 1.0 - p1;
@@ -106,18 +108,25 @@ double GiniImpurity(const vector<int>& labs) {
 double WeightedGini(const vector<int>& left_labs, const vector<int>& right_labs){
     int left_size  = left_labs.size();
     int right_size = right_labs.size();
-    if(left_size == 0 || right_size == 0) return 1.0;
-
+    if(left_size == 0 || right_size == 0) {
+        return 1.0;
+    }
     double total_size = left_size + right_size;
-    return (left_size  / total_size) * GiniImpurity(left_labs)
-         + (right_size / total_size) * GiniImpurity(right_labs);
+    return (left_size  / total_size) * GiniImpurity(left_labs)+ (right_size / total_size) * GiniImpurity(right_labs);
 }
 
-// 多数決ラベルの取得
+// 多数決ラベルの取得<-
 int MajorityLabel(const vector<int>& labs){
     int pos = 0;
-    for(int l : labs) pos += l;
-    return (pos * 2 >= (int)labs.size()) ? 1 : 0;
+    for(int i=0;i<(int)labs.size();i++){
+        pos+=labs[i];
+    }
+    if(pos>=(int)labs.size()/2){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 // 1ノードの学習
@@ -131,23 +140,22 @@ void TrainDecisionNode(const vector<vector<double>>& data, const vector<int>& la
 
     for(int feature_id = 0; feature_id < NUM_FEATURES; feature_id++){
         // 特徴量の値を取り出してソート（パーセンタイル閾値用）
-        vector<double> feature_values;
+        vector<double> feature_values;//特徴量を格納する配列
         for(int i = 0; i < n; i++){
             feature_values.push_back(data[i][feature_id]);
         }
         sort(feature_values.begin(), feature_values.end());
-
         // 1〜99パーセンタイルを閾値候補として評価
         for(int p = 1; p <= 99; p++){
-            int pos_idx = (int)((double)p / 100.0 * (n - 1));
+            int pos_idx = (int)((double)p / 100.0 * (n - 1));//パーセンタイルは,全体の1%目の値,2%目の値,...と値をとってくること
             double threshold = feature_values[pos_idx];
 
             vector<int> left_labs, right_labs;
             for(int j = 0; j < n; j++){
                 if(data[j][feature_id] <= threshold){
-                    left_labs.push_back(labs[j]);
+                    left_labs.push_back(labs[j]);//閾値より低いものは,左
                 }else{
-                    right_labs.push_back(labs[j]);
+                    right_labs.push_back(labs[j]);//閾値より高いものは,右
                 }
             }
 
@@ -160,6 +168,7 @@ void TrainDecisionNode(const vector<vector<double>>& data, const vector<int>& la
                 best_right      = MajorityLabel(right_labs);
             }
         }
+
     }
 
     node.feature_id     = best_feature_id;
@@ -168,16 +177,17 @@ void TrainDecisionNode(const vector<vector<double>>& data, const vector<int>& la
     node.right_class_id = best_right;
 }
 
-// 深さ2の決定木の学習
-void TrainDecisionTree(const vector<vector<double>>& data, const vector<int>& labs, vector<TreeNode>& dtree){
-    // 親ノード(dtree[0])を学習
-    TrainDecisionNode(data, labs, dtree[0]);
+
+// 深さ2の決定木の学習←基本的には,node1を繰り返すだけ。
+void TrainDecisionTree(const vector<vector<double>>& data, const vector<int>& labs, vector<TreeNode>& p_tree){//構造体配列として,[0]は親木, [1]は左の子木, [2]は右の子木
+    // 親ノード(p_tree[0])を学習
+    TrainDecisionNode(data, labs, p_tree[0]);
 
     // 親ノードでデータを左右に分割
     vector<vector<double>> left_data, right_data;
     vector<int>            left_labs, right_labs;
     for(int i = 0; i < (int)data.size(); i++){
-        if(data[i][dtree[0].feature_id] <= dtree[0].threshold){
+        if(data[i][p_tree[0].feature_id] <= p_tree[0].threshold){
             left_data.push_back(data[i]);
             left_labs.push_back(labs[i]);
         }else{
@@ -186,20 +196,35 @@ void TrainDecisionTree(const vector<vector<double>>& data, const vector<int>& la
         }
     }
 
-    // 左子ノード(dtree[1])・右子ノード(dtree[2])を学習
-    TrainDecisionNode(left_data,  left_labs,  dtree[1]);
-    TrainDecisionNode(right_data, right_labs, dtree[2]);
+    // 左子ノード(p_tree[1])・右子ノード(p_tree[2])を学習
+    TrainDecisionNode(left_data,  left_labs,  p_tree[1]);
+    TrainDecisionNode(right_data, right_labs, p_tree[2]);
 }
 
 // 性能指標を表示するヘルパー
 void PrintMetrics(int tp, int fp, int fn, int tn){
     double accuracy  = (double)(tp + tn) / (tp + fp + fn + tn);
-    double precision = (tp + fp > 0) ? (double)tp / (tp + fp) : 0.0;
-    double recall    = (tp + fn > 0) ? (double)tp / (tp + fn) : 0.0;
-    double f1_score  = (precision + recall > 0)
-                       ? 2.0 * precision * recall / (precision + recall) : 0.0;
+    double precision;
+    if(tp+fp>0){
+        precision = (double)tp / (tp + fp);
+    }else{
+        precision = 0.0;
+    }
+    double recall;
+    if(tp+fn>0){
+         recall    = (double)tp / (tp + fn);
+    }else{         
+        recall    = 0.0;
+    }
+        double f1_score;
+    if(precision+recall>0){
+        f1_score= 2.0 * precision * recall / (precision + recall);
+    }else{
+        f1_score= 0.0;
+    }
 
-    cout << fixed << setprecision(4);
+
+    cout << fixed << setprecision(4);//4桁表示
     cout << "Accuracy:  " << accuracy  << "\n"
          << "Precision: " << precision << "\n"
          << "Recall:    " << recall    << "\n"
@@ -212,37 +237,61 @@ void PrintMetrics(int tp, int fp, int fn, int tn){
 // 深さ1の評価
 void Evaluation(const TreeNode& node, const vector<vector<double>>& test_data, const vector<int>& test_labels){
     int tp = 0, fp = 0, fn = 0, tn = 0;
+    int pred;
     for(int i = 0; i < (int)test_data.size(); i++){
-        int pred   = (test_data[i][node.feature_id] <= node.threshold)
-                     ? node.left_class_id : node.right_class_id;
+        if(test_data[i][node.feature_id] <= node.threshold){
+            pred = node.left_class_id;
+        }else{
+            pred = node.right_class_id;
+        }
+
         int actual = test_labels[i];
-        if      (pred == 1 && actual == 1) tp++;
-        else if (pred == 1 && actual == 0) fp++;
-        else if (pred == 0 && actual == 1) fn++;
-        else                               tn++;
+        if (pred == 1 && actual == 1) {
+            tp++;
+        }
+        else if (pred == 1 && actual == 0){ 
+            fp++;
+        }
+        else if (pred == 0 && actual == 1) {
+            fn++;
+        }
+        else {
+            tn++;
+        }
     }
     PrintMetrics(tp, fp, fn, tn);
 }
 
 // 深さ2の評価
-void Evaluation(const vector<TreeNode>& dtree, const vector<vector<double>>& test_data, const vector<int>& test_labels){
+void Evaluation(const vector<TreeNode>& p_tree, const vector<vector<double>>& test_data, const vector<int>& test_labels){
     int tp = 0, fp = 0, fn = 0, tn = 0;
     for(int i = 0; i < (int)test_data.size(); i++){
         int pred;
-        if(test_data[i][dtree[0].feature_id] <= dtree[0].threshold){
-            const TreeNode& child = dtree[1];
-            pred = (test_data[i][child.feature_id] <= child.threshold)
-                   ? child.left_class_id : child.right_class_id;
+        if(test_data[i][p_tree[0].feature_id] <= p_tree[0].threshold){
+            const TreeNode& child = p_tree[1];
+            if(test_data[i][child.feature_id] <= child.threshold){
+                pred = child.left_class_id;
+            } else {
+                pred = child.right_class_id;
+            }
         }else{
-            const TreeNode& child = dtree[2];
-            pred = (test_data[i][child.feature_id] <= child.threshold)
-                   ? child.left_class_id : child.right_class_id;
+            const TreeNode& child = p_tree[2];
+            if(test_data[i][child.feature_id] <= child.threshold){
+                pred = child.left_class_id;
+            } else {
+                pred = child.right_class_id;
+            }
         }
         int actual = test_labels[i];
-        if      (pred == 1 && actual == 1) tp++;
-        else if (pred == 1 && actual == 0) fp++;
-        else if (pred == 0 && actual == 1) fn++;
-        else                               tn++;
+        if      (pred == 1 && actual == 1){
+             tp++;
+        }else if (pred == 1 && actual == 0){
+            fp++;
+        }else if (pred == 0 && actual == 1){
+            fn++;
+        }else{
+            tn++;
+        }
     }
     PrintMetrics(tp, fp, fn, tn);
 }
